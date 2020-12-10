113248cdbc2d297c44c2cd88257b93035c3b26c1
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-12-10 22:51:06 +0300

    Updates

---
+++ precache-manifest.79e5c2307a9af054afc78b5207e25b0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5f3019d4ce47a25b8d02b552fdfb88",
+    "url": "/PaginationNBA/index.html"
+  },
+  {
+    "revision": "bbf6e61f99232d6a9493",
+    "url": "/PaginationNBA/static/css/2.a65e9e88.chunk.css"
+  },
+  {
+    "revision": "8d1f33c50229a4fe4c80",
+    "url": "/PaginationNBA/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bbf6e61f99232d6a9493",
+    "url": "/PaginationNBA/static/js/2.bad3a579.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/PaginationNBA/static/js/2.bad3a579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1f33c50229a4fe4c80",
+    "url": "/PaginationNBA/static/js/main.97b652f9.chunk.js"
+  },
+  {
+    "revision": "4ffff3c1209fea2f6c15",
+    "url": "/PaginationNBA/static/js/runtime-main.88e2a576.js"
+  }
+]);
